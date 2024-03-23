21d067d854eb799bec422ff65a79fac28dac7958
wharvex <t.gudlewski@gmail.com>
2024-03-23 17:23:38 -0400

    first commit

---
+++ legacy_desktop/src/com/badlogic/drop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.drop.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "drop";
+		new LwjglApplication(new Drop(), config);
+	}
+}
